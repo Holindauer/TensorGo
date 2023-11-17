2845ef2c152dcb75c5bea92dbc7bf7bcdb4663ac
Holindauer <holindauer@gmail.com>
2023-11-17 07:09:47 -0800

    rand_tensor() added and batching completed for tensor init funcs

--- Tests/init_tensor_test.go
+++ Tests/init_tensor_test.go
@@ -1,4 +1,4 @@
-package main
+package GLA
 
 // This source file contains tests of tensor creation and initialization
 // functions. The tests are intended to be run with the go test command
@@ -6,11 +6,12 @@ package main
 import (
 	"fmt"
 
+	"testing"
+
 	. "github.com/Holindauer/Go-LinAlg.git/GLA"
-	//"time"
 )
 
-func main() {
+func Test_Tensor_Init(t *testing.T) {
 
 	// Const Tensor Initialziation
 
@@ -75,21 +76,56 @@ func main() {
 	fmt.Println("Expect Tensor Shape [10, 10, 10] --- Actual Shape: ", copy_tensor.Shape)
 	fmt.Println("Expected Sum of Data: 499500 --- Actual Sum of Data: ", copy_tensor.Sum_All())
 	fmt.Println()
+	fmt.Println()
 
-	// Identity Matrix Tensor Initialization (currently there is no batching option)
-
-	// Unbatched Identity Matrix Tensor Initialization (currently there is no batching option)
+	// Unbatched Identity Matrix Tensor Initialization
 	fmt.Println("Unbatched Identity Matrix Tensor Initialization:\n------------------------------------------------")
-	identity_matrix_tensor := Eye(10)
+	identity_matrix_tensor := Eye([]int{10, 10}, false)
 
 	Display_Matrix(identity_matrix_tensor, false)
+	fmt.Println()
+	fmt.Println()
+
+	// Batched Identity Matrix Tensor Initialization
+	fmt.Println("Batched Identity Matrix Tensor Initialization:\n-----------------------------------------------")
+	batched_identity_matrix_tensor := Eye([]int{3, 10, 10}, true)
+
+	Display_Matrix(batched_identity_matrix_tensor, true)
+	fmt.Println()
+	fmt.Println()
 
 	// Gram Matrix Tensor Initialization (currently there is no batching option)
 
 	// Unbatched Gram Matrix Tensor Initialization (currently there is no batching option)
 	fmt.Println("Unbatched Gram Matrix Tensor Initialization:\n---------------------------------------------")
-	gram_matrix_tensor := Gramien_Matrix(Range_Tensor([]int{10, 10}, false))
+	gram_matrix_tensor := Gramien_Matrix(Range_Tensor([]int{10, 10}, false), false)
 
 	Display_Matrix(gram_matrix_tensor, false)
+	fmt.Println()
+	fmt.Println()
 
+	// Batched Gram Matrix Tensor Initialization (currently there is no batching option)
+	fmt.Println("Batched Gram Matrix Tensor Initialization:\n---------------------------------------------")
+	batched_gram_matrix_tensor := Gramien_Matrix(Range_Tensor([]int{3, 10, 10}, true), true)
+
+	Display_Matrix(batched_gram_matrix_tensor, true)
+	fmt.Println()
+	fmt.Println()
+
+	// Unbatched Random Tensor Initialization
+	fmt.Println("Unbatched Random Tensor Initialization:\n--------------------------------------")
+	random_tensor := RandFloat_Tensor([]int{10, 10, 10}, 0, 1, false)
+
+	fmt.Println("Expect Tensor Shape [10, 10, 10] --- Actual Shape: ", random_tensor.Shape)
+	fmt.Println()
+	fmt.Println()
+
+	// Batched Random Tensor Initialization
+	fmt.Println("Batched Random Tensor Initialization:\n-------------------------------------")
+	batched_random_tensor := RandFloat_Tensor([]int{3, 10, 10}, 0, 1, true)
+
+	fmt.Println("Expect Tensor Shape [10, 10, 10] --- Actual Shape: ", batched_random_tensor.Shape)
+	fmt.Println()
+	Display_Matrix(batched_random_tensor, true)
+	fmt.Println()
 }
